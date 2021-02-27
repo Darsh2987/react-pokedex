db82f135ea9570b14a170bbdcee021d56824af40
Darsh <darsh2987@gmail.com>
2021-02-27 19:37:22 +0000

    testing - fetching and rendering api data

--- src/App.js
+++ src/App.js
@@ -7,21 +7,40 @@ function App() {
 
   // Fetch data from API
   useEffect(() => {
-    async function fetchData(i) {
-      const request = await axios(`https://pokeapi.co/api/v2/pokemon/${i}`);
-      setPokemon(request.data.results);
+    async function fetchData() {
+      for (let i = 1; i < 152; i++) {
+        const request = await axios(`https://pokeapi.co/api/v2/pokemon/${i}`);
+        setPokemon((pokemon) => [...pokemon, request.data]);
+      }
     }
 
-    for (let i = 1; i < 151; i++) {
-      fetchData(i);
-    }
+    fetchData();
   }, []);
+
+  // console.log(pokemon);
+
+  // Fetch data from API
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const request = await axios(`https://api.themoviedb.org/3/discover/movie?api_key=f5a07de26f8d5c4478107c654093e6e1&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`);
+  //     setPokemon(request.data.results);
+  //   }
+
+  //   fetchData();
+  // }, []);
 
   console.log(pokemon);
 
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      {pokemon.map((item) => (
+        <div>
+          <h1>{item.id}</h1>
+          <h1>{item.name}</h1>
+          {/* <h1>{item.title}</h1> */}
+          {/* <img src={"https://image.tmdb.org/t/p/w1280" + item.poster_path} alt={item.title} /> */}
+        </div>
+      ))}
     </div>
   );
 }
